fed3df6f9c648f596b0eaca1f357e102e167573d
sooftware <sh951011@gmail.com>
2021-01-25 17:18:42 +0900

    Update conformer

--- conformer/encoder.py
+++ conformer/encoder.py
@@ -65,6 +65,7 @@ class ConformerBlock(nn.Module):
             conv_dropout_p: float = 0.1,
             conv_kernel_size: int = 31,
             half_step_residual: bool = True,
+            device: torch.device = 'cuda',
     ):
         super(ConformerBlock, self).__init__()
         if half_step_residual:
@@ -78,32 +79,32 @@ class ConformerBlock(nn.Module):
                     encoder_dim=encoder_dim,
                     expansion_factor=feed_forward_expansion_factor,
                     dropout_p=feed_forward_dropout_p,
-                ),
+                ).to(device),
                 module_factor=self.feed_forward_residual_factor,
-            ),
+            ).to(device),
             ResidualConnection(
                 module=MultiHeadedSelfAttentionModule(
                     d_model=encoder_dim,
                     num_heads=num_attention_heads,
                     dropout_p=attention_dropout_p,
-                ),
-            ),
+                ).to(device),
+            ).to(device),
             ResidualConnection(
                 module=ConformerConvModule(
                     in_channels=encoder_dim,
                     kernel_size=conv_kernel_size,
                     expansion_factor=conv_expansion_factor,
                     dropout_p=conv_dropout_p,
-                ),
-            ),
+                ).to(device),
+            ).to(device),
             ResidualConnection(
                 module=FeedForwardNet(
                     encoder_dim=encoder_dim,
                     expansion_factor=feed_forward_expansion_factor,
                     dropout_p=feed_forward_dropout_p,
-                ),
+                ).to(device),
                 module_factor=self.feed_forward_residual_factor,
-            ),
+            ).to(device),
             LayerNorm(encoder_dim),
         )
 
@@ -169,6 +170,7 @@ class ConformerEncoder(nn.Module):
             conv_dropout_p=conv_dropout_p,
             conv_kernel_size=conv_kernel_size,
             half_step_residual=half_step_residual,
+            device=device,
         ).to(device) for _ in range(num_layers)]
 
     def forward(self, inputs: Tensor, input_lengths: Tensor) -> Tuple[Tensor, Tensor]:

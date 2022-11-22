e6f126386a22c9eada518bd4d226b632e4f18bec
upskyy <seomk9896@naver.com>
2022-11-23 01:18:11 +0900

    Fix relative shift

--- conformer/attention.py
+++ conformer/attention.py
@@ -19,7 +19,7 @@ import torch.nn.functional as F
 from torch import Tensor
 from typing import Optional
 
-from .embedding import PositionalEncoding
+from .embedding import RelPositionalEncoding
 from .modules import Linear
 
 
@@ -54,7 +54,7 @@ class RelativeMultiHeadAttention(nn.Module):
         self.d_model = d_model
         self.d_head = int(d_model / num_heads)
         self.num_heads = num_heads
-        self.sqrt_dim = math.sqrt(d_model)
+        self.sqrt_dim = math.sqrt(self.d_head)
 
         self.query_proj = Linear(d_model, d_model)
         self.key_proj = Linear(d_model, d_model)
@@ -108,7 +108,7 @@ class RelativeMultiHeadAttention(nn.Module):
         padded_pos_score = torch.cat([zeros, pos_score], dim=-1)
 
         padded_pos_score = padded_pos_score.view(batch_size, num_heads, seq_length2 + 1, seq_length1)
-        pos_score = padded_pos_score[:, :, 1:].view_as(pos_score)
+        pos_score = padded_pos_score[:, :, 1:].view_as(pos_score)[:, :, :, : seq_length2 // 2 + 1]
 
         return pos_score
 
@@ -135,14 +135,14 @@ class MultiHeadedSelfAttentionModule(nn.Module):
     """
     def __init__(self, d_model: int, num_heads: int, dropout_p: float = 0.1):
         super(MultiHeadedSelfAttentionModule, self).__init__()
-        self.positional_encoding = PositionalEncoding(d_model)
+        self.positional_encoding = RelPositionalEncoding(d_model)
         self.layer_norm = nn.LayerNorm(d_model)
         self.attention = RelativeMultiHeadAttention(d_model, num_heads, dropout_p)
         self.dropout = nn.Dropout(p=dropout_p)
 
     def forward(self, inputs: Tensor, mask: Optional[Tensor] = None):
-        batch_size, seq_length, _ = inputs.size()
-        pos_embedding = self.positional_encoding(seq_length)
+        batch_size = inputs.size(0)
+        pos_embedding = self.positional_encoding(inputs)
         pos_embedding = pos_embedding.repeat(batch_size, 1, 1)
 
         inputs = self.layer_norm(inputs)
